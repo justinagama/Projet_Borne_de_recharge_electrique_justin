ec24e80e2809c2d1a637e13f6e0a21a995e12c04
justinagama <agamajustin@gmail.com>
2026-01-05 19:02:22 +0100

    Enhance comments and documentation in prise.h
    Added documentation for functions in prise.h.

--- prise.h
+++ prise.h
@@ -1,13 +1,40 @@
+/**
+ * @file prise.h
+ * @author —
+ * @date —
+ * @brief Interface de gestion de la prise de charge.
+ *
+ * Ce fichier définit les fonctions permettant
+ * de contrôler la prise et la trappe associée
+ * à la borne de recharge.
+ */
+
 #ifndef PRISE_H
 #define PRISE_H
 
 #include <donnees_borne.h>
 #include <memoire_borne.h>
 
-void prise_init (void);
-void verrouiller_trappe (void);
+/**
+ * @brief Initialise la prise et la trappe.
+ */
+void prise_init(void);
+
+/**
+ * @brief Verrouille la trappe de la prise.
+ */
+void verrouiller_trappe(void);
+
+/**
+ * @brief Déverrouille la trappe de la prise.
+ */
 void deverrouiller_trappe(void);
+
+/**
+ * @brief Définit l’état du voyant de la prise.
+ *
+ * @param ledstate État du voyant
+ */
 void prise_set_prise(led ledstate);
 
-
-#endif //PRISE_H
+#endif // PRISE_H
